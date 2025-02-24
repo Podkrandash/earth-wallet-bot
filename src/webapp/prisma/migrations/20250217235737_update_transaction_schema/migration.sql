d1d2947c8675cb78e46f3b0f2690638f6c42fe22
Alex Annenskov <annenskov235@gmail.com>
2025-02-24 13:19:45 +0400

    Remove large RAR file and update gitignore

--- src/webapp/prisma/migrations/20250217235737_update_transaction_schema/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropIndex
-DROP INDEX "Transaction_hash_userId_key";
-
--- AlterTable
-ALTER TABLE "Transaction" ALTER COLUMN "hash" DROP NOT NULL;
--- src/webapp/prisma/migrations/20250218060313_add_usdt_balance/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "Transaction" ADD COLUMN     "token" TEXT NOT NULL DEFAULT 'TON';
-
--- AlterTable
-ALTER TABLE "Wallet" ADD COLUMN     "usdtBalance" DOUBLE PRECISION NOT NULL DEFAULT 0;
